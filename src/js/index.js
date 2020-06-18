f12d7cb42420cf47320b0f5859ad45910daf042d
Vlad <vladprodan6@gmail.com>
2020-06-18 16:44:45 +0300

    +++

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,17 @@ $('#alert').click(() => {
   alert('jQuery works!');
 });
 
+$(document).ready(function() {
+  $('.nav-toggle').click(function() {
+    var collapse_content_selector = $(this).attr('href');
+    var toggle_switch = $(this);
+    $(collapse_content_selector).toggle(function() {
+      if ($(this).css('display') == 'none') {
+        toggle_switch.html('View More');
+      } else {
+        toggle_switch.html('View Less');
+      }
+    });
+  });
+});
 // Your jQuery code
